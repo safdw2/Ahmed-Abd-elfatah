b713986af9b0f9238458d8a48ef6c81bd5c11292
safdw2 <fabrikabrawgwa@gmail.com>
2026-09-16 20:21:42 +0300

    Delete migrations/0003_admin_grants.sql

--- migrations/0003_admin_grants.sql
+++
@@ -1,5 +0,0 @@
--- Move the built-in portal owner from the legacy teacher role to the explicit
--- admin grant required by the Admin Console.
-UPDATE students_table
-SET role = 'admin'
-WHERE phone = 'admin' AND role = 'teacher';
